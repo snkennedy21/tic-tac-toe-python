1bc04c0968961c55bb88fb8c0c9591d2d2462d2f
Sean Kennedy <snkennedy21@gmail.com>
2022-07-10 18:25:59 -0400

    refactored a game_over function

--- game.py
+++ game.py
@@ -27,27 +27,55 @@ def game_over(board, num):
     exit()
 
 
+def is_row_winner(board, row):
+    if row == 1:
+        index = 0
+    if row == 2:
+        index = 3
+    if row == 3:
+        index = 6
+    return board[index] == board[index + 1] and board[index + 1] == board[index + 2]
+
+def is_column_winner(board, column):
+    index = column - 1
+    return board[index] == board[index + 3] and board[index + 3] == board[index + 6]
+
+def is_diagonal_winner(board, diagonal):
+    if diagonal == 1:
+        index = 0
+        return board[index] == board[index + 4] and board[index + 4] == board[index + 8]
+    if diagonal == 2:
+        index = diagonal
+        return board[index] == board[index + 2] and board[index + 2] == board[index + 4]
+
 for move_number in range(1, 10):
     print_board(board)
     response = input("Where would " + current_player + " like to move? ")
     space_number = int(response) - 1
     board[space_number] = current_player
 
-    if board[0] == board[1] and board[1] == board[2]:
+    if is_row_winner(board, 1):
         game_over(board, 0)
-    elif board[3] == board[4] and board[4] == board[5]:
+
+    elif is_row_winner(board, 2):
         game_over(board, 3)
-    elif board[6] == board[7] and board[7] == board[8]:
+
+    elif is_row_winner(board, 3):
         game_over(board, 6)
-    elif board[0] == board[3] and board[3] == board[6]:
+
+    elif is_column_winner(board, 1):
         game_over(board, 0)
-    elif board[1] == board[4] and board[4] == board[7]:
+
+    elif is_column_winner(board, 2):
         game_over(board, 1)
-    elif board[2] == board[5] and board[5] == board[8]:
+
+    elif is_column_winner(board, 3):
         game_over(board, 2)
-    elif board[0] == board[4] and board[4] == board[8]:
+
+    elif is_diagonal_winner(board, 1):
         game_over(board, 0)
-    elif board[2] == board[4] and board[4] == board[6]:
+
+    elif is_diagonal_winner(board, 2):
         game_over(board, 2)
 
     if current_player == "X":
